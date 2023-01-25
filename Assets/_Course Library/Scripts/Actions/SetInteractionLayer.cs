38d2fcefee4f3b761449bf8ecdb0676610f89d0f
StoneMooseGames <61326650+StoneMooseGames@users.noreply.github.com>
2023-01-25 19:33:59 +0200

    CreateWithVR added as playground

---
+++ Assets/_Course Library/Scripts/Actions/SetInteractionLayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// Set the interaction layer of an interactor
+/// </summary>
+public class SetInteractionLayer : MonoBehaviour
+{
+    [Tooltip("The layer that's switched to")]
+    public LayerMask targetLayer = 0;
+
+    private XRBaseInteractor interactor = null;
+    private LayerMask originalLayer = 0;
+
+    private void Awake()
+    {
+        interactor = GetComponent<XRBaseInteractor>();
+        originalLayer = interactor.interactionLayers.value;
+    }
+
+    public void SetTargetLayer()
+    {
+        interactor.interactionLayers = targetLayer.value;
+    }
+
+    public void SetOriginalLayer()
+    {
+        interactor.interactionLayers = originalLayer.value;
+    }
+
+    public void ToggleTargetLayer(bool value)
+    {
+        if (value)
+        {
+            SetTargetLayer();
+        }
+        else
+        {
+            SetOriginalLayer();
+        }
+    }
+
+}
