bd0f5047204b4c09605d3761afbfba787bf0c14c
ducanhxh <62750101+ducanhxh@users.noreply.github.com>
2023-01-26 13:15:01 +0200

    Teleportation (and unknown changes)

---
+++ Assets/__GROUP FOLDER/Scripts/LookAtTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    
+     public Transform target;
+
+
+    void Update()
+    {
+        if (target != null)
+        {
+            transform.LookAt(target);
+        }
+    }
+
+}
+
