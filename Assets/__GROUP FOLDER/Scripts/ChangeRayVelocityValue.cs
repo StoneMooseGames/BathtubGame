8bc5c6c3e8711565bf00ec2391fcd1a6350977cb
ducanhxh <62750101+ducanhxh@users.noreply.github.com>
2023-01-26 16:25:05 +0200

    Teleportation basics DONE
    Needs still some tweaking and finer adjusting?

---
+++ Assets/__GROUP FOLDER/Scripts/ChangeRayVelocityValue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ChangeRayVelocityValue : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 newValue;
+    private float oldValue;
+
+
+
+
+    private void Start()
+    {
+        //oldScale = this.gameObject.GetComponent<>
+    }
+
+
+    public void ApplyNewScale()
+    {
+       // target.transform.localScale = newScale;
+    }
+
+
+
+    public void ApplyOldScale()
+    {
+        //target.transform.localScale = oldScale;
+    }
+}
